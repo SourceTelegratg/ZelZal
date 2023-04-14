8f9e44c4ddf5e5eebd26232ca1468ea7e615c631
SourceTelegratg <130600739+SourceTelegratg@users.noreply.github.com>
2023-04-14 03:20:43 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -31,7 +31,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Telegratg/Zelzal"
+        "UPSTREAM_REPO", "https://github.com/SourceTelegratg/Zelzal"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
@@ -182,9 +182,9 @@ class Config(object):
     BOTLOG_CHATID = 0
     # extra plugins realted vars below  4
     EXTERNAL_REPOBRANCH = os.environ.get("EXTERNAL_REPOBRANCH", "main")
-    OLDZED_REPO = os.environ.get("OLDZED_REPO", "https://github.com/Telegratg/ZED_USERBOT")
+    OLDZED_REPO = os.environ.get("OLDZED_REPO", "https://github.com/SourceTelegratg/ZED_USERBOT")
     if OLDZED_REPO and not url(OLDZED_REPO):
-        OLDZED_REPO = "https://github.com/Telegratg/ZED_USERBOT"
+        OLDZED_REPO = "https://github.com/SourceTelegratg/ZED_USERBOT"
     OLDZED_REPOBRANCH = os.environ.get("OLDZED_REPOBRANCH", "oldzed")
 
 
